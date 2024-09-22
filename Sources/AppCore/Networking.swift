45c2f5b03ead88b391166e886259cc4b393d3809
sariweb <ariweb@ya.ru>
2024-09-22 11:44:32 +0500

    Refactored first Networking API to include dependency injection for easier testing.

--- Sources/AppCore/Networking.swift
+++ Sources/AppCore/Networking.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+protocol NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void)
+}
+
+extension URLSession: NetworkSession {
+    func get(from url: URL, completionHandler: @escaping (Data?, (any Error)?) -> Void) {
+        let task = dataTask(with: url) { data, _, error in
+            completionHandler(data, error)
+        }
+        task.resume()
+    }
+    
+    
+}
+
 extension AppCore {
     public class Networking {
         /// Responsible for handling all networking calls
@@ -14,14 +29,17 @@ extension AppCore {
         public class Manager {
             public init() {}
             
-            private let session = URLSession.shared
+            internal var session: NetworkSession = URLSession.shared
             
+            /// Calls to the live internet to retrieve data from a specific location
+            /// - Parameters:
+            ///   - url: The location you wish to fetch from
+            ///   - completionHandler: Returns a result object which signifies the status of the request
             public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
-                let task = session.dataTask(with: url) { data, response, error in
+                session.get(from: url) { data, error in
                     let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
                     completionHandler(result)
                 }
-                task.resume()
             }
         }
         
--- Tests/AppCoreTests/AppNetworkingTests.swift
+++ Tests/AppCoreTests/AppNetworkingTests.swift
@@ -8,15 +8,27 @@
 import XCTest
 @testable import AppCore
 
+class NetworkSessionMock: NetworkSession {
+    var data: Data?
+    var error: Error?
+    
+    func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+    
+    
+}
+
 final class AppNetworkingTests: XCTestCase {
 
     func testLoadDataCall() {
         let manager = AppCore.Networking.Manager()
+        let session = NetworkSessionMock()
+        manager.session = session
         let expectation = XCTestExpectation(description: "Called for data")
-        guard let url = URL(string: "https://kodeco.com") else {
-            XCTFail("Could not create URL properly")
-            return
-        }
+        let data = Data([0, 1, 0, 1])
+        session.data = data
+        let url = URL(fileURLWithPath: "url")
         manager.loadData(from: url) { result in
             expectation.fulfill()
             switch result {
